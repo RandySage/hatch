3fcb81a90138006f6d6a97e3c1981edfdeb53727
NA <NA>
2013-12-25 18:52:56 -0800

    finally limited fields shown on admin

--- simplelist/admin.py
+++ simplelist/admin.py
@@ -2,23 +2,24 @@ from simplelist.models import List, Entry
 from django.contrib import admin
 from django.utils import timezone
 
-class ListAdmin:
-    fields = ('list_name',)
+class ListAdmin(admin.ModelAdmin):
+    exclude = ('created_by','create_date',)
 
     def save_model(self, request, obj, form, change):
         obj.created_by = request.user
         obj.create_date = timezone.now()
         obj.save()
-    pass
 
-class EntryAdmin:
-    fields = ('list', 'heading_text', 'body_text', 'doc_sort_order',)
+
+class EntryAdmin(admin.ModelAdmin):
+    exclude = ('created_by','create_date',)
+    #fields = ('list', 'heading_text', 'body_text', 'doc_sort_order',)
 
     def save_model(self, request, obj, form, change):
         obj.created_by = request.user
         obj.create_date = timezone.now()
         obj.save()
-    pass
+
 
 admin.site.register(List, ListAdmin)
 admin.site.register(Entry, EntryAdmin)
