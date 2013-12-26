8e796e9bb93cde77c1a656699a15dd39045ab869
NA <NA>
2013-12-25 17:18:53 -0800

    Hack fest - probably stepped on protected term list

--- simplelist/admin.py
+++ simplelist/admin.py
@@ -1,6 +1,24 @@
-from django.contrib import admin
-
 from simplelist.models import List, Entry
+from django.contrib import admin
+from django.utils import timezone
 
-admin.site.register(List)
-admin.site.register(Entry)
+class ListAdmin:
+    fields = ('list_name',)
+
+    def save_model(self, request, obj, form, change):
+        obj.created_by = request.user
+        obj.create_date = timezone.now()
+        obj.save()
+    pass
+
+class EntryAdmin:
+    fields = ('list', 'heading_text', 'body_text', 'doc_sort_order',)
+
+    def save_model(self, request, obj, form, change):
+        obj.created_by = request.user
+        obj.create_date = timezone.now()
+        obj.save()
+    pass
+
+admin.site.register(List, ListAdmin)
+admin.site.register(Entry, EntryAdmin)
--- simplelist/models.py
+++ simplelist/models.py
@@ -1,26 +1,24 @@
 from django.db import models
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 
 import datetime
 from django.utils import timezone
 
 class List(models.Model):
-    spec_name = models.CharField(max_length=200)
+    list_name = models.CharField(max_length=200)
     create_date = models.DateTimeField('date created')
-
+    created_by = models.ForeignKey(User)
     # This function is maintained only as a template for later functions
-    def was_created_recently(self):
-        #return self.create_date >= timezone.now() - datetime.timedelta(days=1)
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.create_date < now
-
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.spec_name
 
 class Entry(models.Model):
     MAX_SHORT_TEXT_LENGTH = 55;
 
-    spec = models.ForeignKey(List)
+    list = models.ForeignKey(List)
+    create_date = models.DateTimeField('date created')
+    created_by = models.ForeignKey(User)
     heading_text = models.CharField(max_length=200, blank=True)
     body_text = models.CharField(max_length=2000, blank=True)
     doc_sort_order = models.IntegerField(default=0, null=False)
